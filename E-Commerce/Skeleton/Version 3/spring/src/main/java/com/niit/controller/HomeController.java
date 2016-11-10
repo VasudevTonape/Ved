17e66d6a259fd57670e316d7fd29a38ab9487eff
VasudevTonape <vasudevtonpe@gmail.com>
2016-11-10 14:24:37 +0530

    Version 3

---
+++ E-Commerce/Skeleton/Version 3/spring/src/main/java/com/niit/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.niit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "home")
+	public String homePage() {
+		System.out.println("home page !");
+		return "home";
+	}
+
+	@RequestMapping(value = "/about")
+	public String aboutUs() {
+		return "about";
+	}
+}
